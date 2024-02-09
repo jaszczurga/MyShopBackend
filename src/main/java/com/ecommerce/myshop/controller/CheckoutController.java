a5e9988e450cbb942b9d7589c00c653e0dba6bf8
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2024-02-09 21:37:11 +0100

    finised initial stripe payments and placing orders

--- src/main/java/com/ecommerce/myshop/controller/CheckoutController.java
+++ src/main/java/com/ecommerce/myshop/controller/CheckoutController.java
@@ -9,13 +9,11 @@ import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/checkout")
+@CrossOrigin (origins = "http://localhost:4200")
 public class CheckoutController {
 
     private CheckoutService checkoutService;
