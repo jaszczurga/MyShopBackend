d8005becde0f6fba5b23f373453a16663f187290
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2024-02-13 23:55:03 +0100

    changed SecurityConfiguration in order to have access to api/action/** endpoints with get method

--- src/main/java/com/ecommerce/myshop/config/Authentication/SecurityConfiguration.java
+++ src/main/java/com/ecommerce/myshop/config/Authentication/SecurityConfiguration.java
@@ -3,13 +3,14 @@ package com.ecommerce.myshop.config.Authentication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @Configuration
@@ -25,8 +26,10 @@ public class SecurityConfiguration {
 
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeRequests(authorize -> authorize
-                        .requestMatchers( "/api/v1/auth/**" )
+                .authorizeRequests((authorize)-> authorize
+                        .requestMatchers(  "/api/v1/auth/**" )
+                        .permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/action/**" )
                         .permitAll()
                         .anyRequest()
                         .authenticated())
