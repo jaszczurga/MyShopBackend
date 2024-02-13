fe73c9e9e1ee593df2f219cf4106fb80a41a160d
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2024-02-13 18:38:10 +0100

    created JwtAuthenticationFilter

---
+++ src/main/java/com/ecommerce/myshop/config/Authentication/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.ecommerce.myshop.config.Authentication;
+
+
+import com.ecommerce.myshop.service.Authentication.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+//every request will go through this filter
+//we extends so we use some ready made methods
+//we checks if the request has a valid token
+@Component
+@RequiredArgsConstructor //lombok will create a constructor with all final fields
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request ,
+            @NonNull HttpServletResponse response ,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if(authHeader==null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        //if the token is valid, we will set the authentication object in the security context 7 because Bearer is 7 characters long
+        jwt = authHeader.substring( 7 );
+        userEmail = jwtService.extractUsername(jwt);
+        if(userEmail!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+            if(jwtService.isTokenValid(jwt,userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource()
+                                .buildDetails( request )
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+            filterChain.doFilter(request,response);
+
+        }
+    }
+}
